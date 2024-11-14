1378daf1dc3f1bb7b67df8faa2da370645e20c0b
Manuel Bracher <m.bracher@fit-ebike.ch>
2024-11-14 14:18:52 +0100

    New version v20241114

--- Qlockwork.ino
+++ Qlockwork.ino
@@ -20,7 +20,7 @@
 //
 //*****************************************************************************
 
-#define FIRMWARE_VERSION 20240227
+#define FIRMWARE_VERSION 20241114
 
 #include <Arduino.h>
 #include <Arduino_JSON.h>
